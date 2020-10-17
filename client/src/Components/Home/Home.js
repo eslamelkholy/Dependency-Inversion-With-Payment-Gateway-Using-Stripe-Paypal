7f0d543a78c5f2801f6fbb9971a5f94399704237
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-10-17 18:14:34 +0200

    Setup Simple Payment Form With React Using Hooks

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = (props) => {
+  const [payment, setPayment] = useState({
+    price: 0,
+  });
+
+  const handleInputChange = (e) => setPayment({ ...payment, [e.target.name]: e.target.value });
+
+  const submitPayment = (e) => {
+    e.preventDefault();
+    console.log(payment);
+  };
   return (
     <div>
       <h1>Hello This Demo For Online Payment</h1>
-      <h2>Buy For 200$</h2>
+      <form onSubmit={submitPayment}>
+        <label>
+          Price:
+          <input name="price" type="number" value={payment.price} onChange={handleInputChange} />
+        </label>
+        <input type="submit" />
+      </form>
     </div>
   );
 };
